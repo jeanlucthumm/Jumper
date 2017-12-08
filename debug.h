fb3021b809829d8f17972e3d69047b275bfddb05
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-12-07 20:05:39 -0800

    Add utility debug header

---
+++ debug.h
@@ -0,0 +1,13 @@
+//
+// Created by Jean-Luc Thumm on 12/7/17.
+//
+
+#ifndef JUMPER_DEBUG_H
+#define JUMPER_DEBUG_H
+
+#include "util.h"
+#include <iostream>
+
+using namespace std;
+
+#endif //JUMPER_DEBUG_H
