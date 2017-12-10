8a27075a53634da6799834f88b190b4e8c9f3877
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-12-09 17:20:14 -0800

    Add DirLight class

---
+++ DirLight.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Jean-Luc Thumm on 12/9/17.
+//
+#include "DirLight.hpp"
+
+
+void DirLight::attach(const std::shared_ptr<Shader> &shader) {
+    shaders.insert(shader);
+}
+
+void DirLight::detach(const std::shared_ptr<Shader> &shader) {
+    shaders.erase(shader);
+}
+
+void DirLight::draw(const glm::mat4 &parent, const glm::mat4 &view,
+                    const glm::mat4 &projection) {
+    for (auto &shader : shaders) {
+        shader->put(tag + ".direction", direction);
+        shader->put(tag + ".ambient", ambient);
+        shader->put(tag + ".diffuse", diffuse);
+        shader->put(tag + ".specular", specular);
+    }
+}
+
+void DirLight::update() {}
+
+std::list<Node *> DirLight::hit(const Ray &ray) {
+    return std::list<Node *>{};
+}
+
+DirLight::DirLight(glm::vec3 direction, glm::vec3 color, size_t shaderArrayPos)
+        : direction{direction}, ambient{color}, diffuse{color}, specular{color} {
+    tag = GLSL_ARRAY_NAME + "[" + std::to_string(shaderArrayPos) + "]";
+}
---
+++ DirLight.hpp
@@ -0,0 +1,41 @@
+//
+// Created by Jean-Luc Thumm on 12/9/17.
+//
+
+#ifndef JUMPER_DIRLIGHT_HPP
+#define JUMPER_DIRLIGHT_HPP
+
+#include "Node.h"
+#include "Shader.h"
+#include <glm/vec3.hpp>
+#include <memory>
+#include <unordered_set>
+#include <string>
+
+class DirLight : public Node {
+public:
+    constexpr std::string GLSL_ARRAY_NAME = "dirLights";
+
+    DirLight(glm::vec3 direction,
+             glm::vec3 color,
+             size_t shaderArrayPos);
+
+    void attach(const std::shared_ptr<Shader> &shader);
+    void detach(const std::shared_ptr<Shader> &shader);
+
+    void draw(const glm::mat4 &parent, const glm::mat4 &view,
+              const glm::mat4 &projection) override;
+    void update() override;
+    std::list<Node *> hit(const Ray &ray) override;
+
+private:
+    glm::vec3 direction;
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    std::string tag;
+
+    std::unordered_set<std::shared_ptr<Shader>> shaders;
+};
+
+#endif //JUMPER_DIRLIGHT_HPP
